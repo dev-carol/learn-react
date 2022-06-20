a35b2dc50ff35ee01bb7ddc015825843c11518eb
carol santos <contato.caroline1999@gmail.com>
2022-06-20 15:27:31 -0300

    feat: styles to card

---
+++ src/components/Card/index.jsx
@@ -0,0 +1,10 @@
+import './styles.css'
+
+export function Card(){
+    return(
+        <div className='card'>
+            <strong>Carol Santos</strong>
+            <small>13:05:20</small>
+        </div>
+    )
+}
